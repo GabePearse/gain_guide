96c95fb5fc0394dc767b65f07cf999ae4ed87a2d
Gabriel Pearse <gabe@pearse.name>
2026-09-17 18:14:03 -0600

    Add exercise targets and rest metadata

--- lib/models/exercise.dart
+++ lib/models/exercise.dart
@@ -5,12 +5,22 @@ class Exercise {
   final int workoutId;
   final String name;
   final List<SetEntry> sets;
+  final int targetSets;
+  final int minReps;
+  final int maxReps;
+  final int restMinSeconds;
+  final int restMaxSeconds;
 
   const Exercise({
     this.id,
     required this.workoutId,
     required this.name,
     this.sets = const [],
+    this.targetSets = 3,
+    this.minReps = 8,
+    this.maxReps = 10,
+    this.restMinSeconds = 90,
+    this.restMaxSeconds = 90,
   });
 
   Exercise copyWith({
@@ -18,12 +28,22 @@ class Exercise {
     int? workoutId,
     String? name,
     List<SetEntry>? sets,
+    int? targetSets,
+    int? minReps,
+    int? maxReps,
+    int? restMinSeconds,
+    int? restMaxSeconds,
   }) {
     return Exercise(
       id: id ?? this.id,
       workoutId: workoutId ?? this.workoutId,
       name: name ?? this.name,
       sets: sets ?? this.sets,
+      targetSets: targetSets ?? this.targetSets,
+      minReps: minReps ?? this.minReps,
+      maxReps: maxReps ?? this.maxReps,
+      restMinSeconds: restMinSeconds ?? this.restMinSeconds,
+      restMaxSeconds: restMaxSeconds ?? this.restMaxSeconds,
     );
   }
 
@@ -32,6 +52,11 @@ class Exercise {
       'id': id,
       'workoutId': workoutId,
       'name': name,
+      'targetSets': targetSets,
+      'minReps': minReps,
+      'maxReps': maxReps,
+      'restMinSeconds': restMinSeconds,
+      'restMaxSeconds': restMaxSeconds,
     };
   }
 
@@ -40,6 +65,11 @@ class Exercise {
       id: map['id'] as int?,
       workoutId: map['workoutId'] as int,
       name: map['name'] as String,
+      targetSets: (map['targetSets'] as int?) ?? 3,
+      minReps: (map['minReps'] as int?) ?? 8,
+      maxReps: (map['maxReps'] as int?) ?? 10,
+      restMinSeconds: (map['restMinSeconds'] as int?) ?? 90,
+      restMaxSeconds: (map['restMaxSeconds'] as int?) ?? 90,
     );
   }
 }
